d30c25a498f8d1159dcdc470597e5736d46573fb
BarbzYHOOL <professor_barbz@gmx.com>
2020-02-01 20:51:38 +0100

    chore: Moved SQL again

--- sql/world/base/structure.sql
+++
@@ -1,9 +0,0 @@
--- COMMANDS
-
-SET @REQUIRED_GMLEVEL = 3;
-
-INSERT INTO `command` VALUES ('morphall', @REQUIRED_GMLEVEL, 'Syntax: .morphall #displayid \r\n\r\nChange players model id to #displayid.');
-INSERT INTO `command` VALUES ('demorphall', @REQUIRED_GMLEVEL, 'Syntax: .demorphall #displayid \r\n\r\nDemorph all players.');
-
-
-INSERT INTO `acore_string` VALUES (40000, 'Reloaded morph_all_command', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
